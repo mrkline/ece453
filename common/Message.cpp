d304480a53f57fde71edfadd3283088fb7e2474e
Matt Kline <slavik262@gmail.com>
2014-03-31 12:47:34 -0500

    Minor Message -> JSON cleanup

--- common/Message.cpp
+++ common/Message.cpp
@@ -44,17 +44,19 @@ std::unique_ptr<Message> Message::fromJSON(const Json::Value& object)
 
 Json::Value Message::toJSON() const
 {
+	using Type = Message::Type;
+
 	static const std::unordered_map<Message::Type, StaticString> nameLookup = {
-		{Message::Type::EMPTY, StaticString("empty")},
-		{Message::Type::RESPONSE, StaticString("response")},
-		{Message::Type::SETUP, StaticString("setup")},
-		{Message::Type::START, StaticString("start")},
-		{Message::Type::STOP, StaticString("stop")},
-		{Message::Type::STATUS, StaticString("status")},
-		{Message::Type::STATUS_RESPONSE, StaticString("status response")},
-		{Message::Type::RESULTS, StaticString("results")},
-		{Message::Type::RESULTS_RESPONSE, StaticString("results response")},
-		{Message::Type::TEST, StaticString("test")}
+		{Type::EMPTY, StaticString("empty")},
+		{Type::RESPONSE, StaticString("response")},
+		{Type::SETUP, StaticString("setup")},
+		{Type::START, StaticString("start")},
+		{Type::STOP, StaticString("stop")},
+		{Type::STATUS, StaticString("status")},
+		{Type::STATUS_RESPONSE, StaticString("status response")},
+		{Type::RESULTS, StaticString("results")},
+		{Type::RESULTS_RESPONSE, StaticString("results response")},
+		{Type::TEST, StaticString("test")}
 		// {Message::Type::UNKNOWN, "unknown"}
 	};
 
@@ -71,17 +73,19 @@ bool Message::operator==(const Message& o) const
 
 std::unique_ptr<Message> JSONToMessage(const Json::Value& object)
 {
+	using Type = Message::Type;
+
 	static const std::unordered_map<std::string, Message::Type> typeLookup = {
-		{"empty", Message::Type::EMPTY},
-		{"response", Message::Type::RESPONSE},
-		{"setup", Message::Type::SETUP},
-		{"start", Message::Type::START},
-		{"stop", Message::Type::STOP},
-		{"status", Message::Type::STATUS},
-		{"status response", Message::Type::STATUS_RESPONSE},
-		{"results", Message::Type::RESULTS},
-		{"results response", Message::Type::RESULTS_RESPONSE},
-		{"test", Message::Type::TEST}
+		{"empty", Type::EMPTY},
+		{"response", Type::RESPONSE},
+		{"setup", Type::SETUP},
+		{"start", Type::START},
+		{"stop", Type::STOP},
+		{"status", Type::STATUS},
+		{"status response", Type::STATUS_RESPONSE},
+		{"results", Type::RESULTS},
+		{"results response", Type::RESULTS_RESPONSE},
+		{"test", Type::TEST}
 		// {"unknown", Message::Type::UNKNOWN}
 	};
 
