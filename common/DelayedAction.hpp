4e4efa25474ad8c8b9eea4b4f71e08dc52de8527
Matt Kline <slavik262@gmail.com>
2014-04-07 23:02:09 -0500

    Added DelayedAction
    This can be used to set an action that will be excecuted
    after a given amount of time.
    For example, we can set up an end-game event when we start the game.

---
+++ common/DelayedAction.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+
+#include <type_traits>
+
+/**
+ * \brief Fires off a some action after a given amount of time
+ * \tparam A The action to take
+ */
+template <typename T = std::chrono::milliseconds>
+class DelayedAction  {
+public:
+	template <typename A>
+	DelayedAction(T del, A act) :
+		over(false),
+		mtx(),
+		cv(),
+		delay(del),
+		action(act),
+		worker(&DelayedAction::threadProc, this)
+	{ }
+
+	~DelayedAction()
+	{
+		// If we haven't exited yet, do it now
+		runEarly();
+		worker.join();
+	}
+
+	void runEarly()
+	{
+		std::lock_guard<std::mutex> guard(mtx);
+		over = true;
+	}
+
+	bool isOver()
+	{
+		std::lock_guard<std::mutex> guard(mtx);
+		return over;
+	}
+	
+private:
+
+	void threadProc()
+	{
+		std::unique_lock<std::mutex> lock(mtx);
+		if (!cv.wait_for(lock, delay, [&] { return over; }))
+			over = true; // If we weren't over yet, we're over now.
+		action();
+	}
+
+	bool over;
+	std::mutex mtx;
+	std::condition_variable cv;
+	T delay;
+	std::function<void()> action;
+	std::thread worker;
+};
