ac916c32596f60fe8b0312d6fa938df67742e09b
Matt Kline <slavik262@gmail.com>
2014-04-25 17:55:03 -0500

    Filled in setup section of UI

--- ui/RangeUI.cpp
+++ ui/RangeUI.cpp
@@ -3,11 +3,23 @@
 
 using namespace std;
 
+
+
 RangeUI::RangeUI(QWidget *parent) :
 	QMainWindow(parent),
 	ui(new Ui::RangeUI)
 {
 	ui->setupUi(this);
+	// Have the checkboxes for win conditions enable and disable
+	// their corresponding spin boxes and labels
+	connect(ui->chkTime, &QCheckBox::stateChanged, [this](int checked) {
+		ui->spnTime->setEnabled(checked == Qt::Checked);
+		ui->lblTime->setEnabled(checked == Qt::Checked);
+	});
+	connect(ui->chkScore, &QCheckBox::stateChanged, [this](int checked) {
+		ui->spnScore->setEnabled(checked == Qt::Checked);
+		ui->lblScore->setEnabled(checked == Qt::Checked);
+	});
 }
 
 RangeUI::~RangeUI()
