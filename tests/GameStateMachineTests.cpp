4b500dd2c10721245e2e270bfead6bdc95bbf9c8
Matt Kline <slavik262@gmail.com>
2014-04-25 01:00:32 -0500

    Improved and fixed test macros
    We need to assign the macro argument to a variable,
    as if it is a function, it gets called every time we mention it.
    Yay macros.

--- tests/GameStateMachineTests.cpp
+++ tests/GameStateMachineTests.cpp
@@ -21,17 +21,20 @@ using namespace std;
 /// Macro to quickly set up a test environment for the game state machine(s)
 #define MACHINE_ENVIRONMENT \
 	MessageQueue in, out; \
-	thread stateThread(&runGame, ref(in), ref(out), 2, 2);
+	thread stateThread(&runGame, ref(in), ref(out), 2, 2); \
+	int id = -1; \
+	(void)id; // No unused warnings please
 
 /// Tests that the output queue from MACHINE_ENVIRONMENT is empty
 #define ASSERT_EMPTY_OUT \
 	assert(out.empty());
 
 /// Macro to send a thread to the input queue from MACHINE_ENVIRONMENT
-#define SEND(msg) \
+#define SEND(msg) { \
 	auto toSend = msg; \
-	const auto id = toSend->id; \
-	in.send(move(msg));
+	id = toSend->id; \
+	in.send(move(msg)); \
+}
 
 /// Macro to join the thread from MACHINE_ENVIRONMENT to exit
 #define EXIT \
