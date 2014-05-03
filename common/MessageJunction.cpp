85be73b68f0810d7b20d2ee072d7d855436a0a11
Matt Kline <slavik262@gmail.com>
2014-05-02 23:46:35 -0500

    Wrote initial draft of the message junction

--- common/MessageJunction.cpp
+++ common/MessageJunction.cpp
@@ -1,8 +1,14 @@
 #include "MessageJunction.hpp"
+#include "MemoryUtils.hpp"
+
+#include "Exceptions.hpp"
+#include "ResponseMessage.hpp"
 
 #include <chrono>
 
+using namespace std;
 using namespace std::chrono;
+using namespace Exceptions;
 
 typedef std::chrono::steady_clock Clock;
 
@@ -20,5 +26,45 @@ void runMessageJunction(MessageQueue& toSM, MessageQueue& fromSM,
 		const auto stateMachineEnd = start + timeSlice;
 		const auto userInterfaceEnd = stateMachineEnd + timeSlice;
 		const auto systemEnd = userInterfaceEnd + timeSlice;
+
+		for (auto msg = fromSM.receiveUntil(stateMachineEnd);
+			msg != nullptr;
+			msg = fromSM.receiveUntil(stateMachineEnd)) {
+
+			// Try casting to a response.
+			// Responses from the state machine are going to the UI.
+			// Otherwise they are commands and go to the system
+			auto response = unique_dynamic_cast<ResponseMessage>(move(msg));
+
+			if (response != nullptr)
+				toUI.send(move(response));
+			else
+				toSys.send(move(msg));
+		}
+
+		for (auto msg = fromUI.receiveUntil(userInterfaceEnd);
+			msg != nullptr;
+			msg = fromUI.receiveUntil(stateMachineEnd)) {
+
+			// Try casting to a response.
+			// Things shouldn't be sending messages to the UI,
+			// so it shouldn't be responding.
+			// Messages from the UI go to the state machine
+			auto response = unique_dynamic_cast<ResponseMessage>(move(msg));
+
+			if (response != nullptr)
+				THROW(InvalidOperationException, "Response from UI: " + response->message);
+			else
+				toSM.send(move(msg));
+		}
+
+		for (auto msg = fromSys.receiveUntil(systemEnd);
+			msg != nullptr;
+			msg = fromSys.receiveUntil(systemEnd)) {
+
+			// Messages from the system go to the state machine.
+			// TODO: Make a copy and send it to the UI?
+			toSM.send(move(msg));
+		}
 	}
 }
